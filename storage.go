b4b810f6b828e03ee2ebcc18cf88a4c85ee0cb96
Victoria Odalo <147773886+OdaloV@users.noreply.github.com>
2026-06-17 10:55:06 +0000

    feat: implement JSON load and save logic for tasks

---
+++ storage.go
@@ -0,0 +1,56 @@
+func loadTask(filename string) ([]Task, error) {
+	//check if file exists
+	_, err := os.Lstat(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return []Task{}, nil
+		}
+		return nil, err
+	}
+	//open
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	//read file
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(fileData) == 0 {
+		return []Task{}, nil
+	}
+
+	//parse json data to something go can read
+	var tasklist []Task
+	err = json.NewDecoder(file).Decode(&tasklist)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) || err.Error() == "EOF" {
+			return []Task{}, nil
+		}
+		return nil, err
+	}
+
+	return tasklist, nil
+
+}
+
+func saveTask(filename string, tasklist []Task) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	if err := encoder.Encode(tasklist); err != nil {
+		return err
+	}
+
+	return nil
+}
