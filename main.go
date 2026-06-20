2704594210e575b26e46d97ac5345c0446804a7e
OdaloV <odalojanetvicky@gmail.com>
2026-06-20 15:11:41 +0300

    fix:merge conflict

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
-import(
+import (
 	"fmt"
 	"os"
 )
 
-func main(){
+func main() {
 	if len(os.Args) < 2 {
 		fmt.Print("Please input an arguement")
 		return
 	}
+
 	command := os.Args[1]
 
-	
+	switch command {
+	case "add":
 
+	case "list":
+	case "update":
+	case "delete":
+	case "in progress":
+	case "done":
+	default:
+
+	}
 
 }
