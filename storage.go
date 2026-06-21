ffec19dbe9c496e4d0d219bf9e0bb44fc51ca271
OdaloV <odalojanetvicky@gmail.com>
2026-06-21 10:46:42 +0300

    fix(storage): fix JSON parsing with json.Unmarshal and improve error messages

--- storage.go
+++ storage.go
@@ -3,54 +3,41 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"io"
+	"fmt"
 	"os"
 )
 
 func loadTask(filename string) ([]Task, error) {
-	//check if file exists
 	_, err := os.Lstat(filename)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return []Task{}, nil
 		}
-		return nil, err
+		return nil, fmt.Errorf("failed to access file: %w", err)
 	}
-	//open
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
 
-	//read file
-	fileData, err := io.ReadAll(file)
+	fileData, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read file: %w", err)
 	}
 
 	if len(fileData) == 0 {
 		return []Task{}, nil
 	}
 
-	//parse json data to something go can read
 	var tasklist []Task
-	err = json.NewDecoder(file).Decode(&tasklist)
+	err = json.Unmarshal(fileData, &tasklist)
 	if err != nil {
-		if errors.Is(err, os.ErrNotExist) || err.Error() == "EOF" {
-			return []Task{}, nil
-		}
-		return nil, err
+		return nil, fmt.Errorf("corrupted JSON file: %w", err)
 	}
 
 	return tasklist, nil
-
 }
 
 func saveTask(filename string, tasklist []Task) error {
 	file, err := os.Create(filename)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create file: %w", err)
 	}
 	defer file.Close()
 
@@ -58,7 +45,7 @@ func saveTask(filename string, tasklist []Task) error {
 	encoder.SetIndent("", "  ")
 
 	if err := encoder.Encode(tasklist); err != nil {
-		return err
+		return fmt.Errorf("failed to encode JSON: %w", err)
 	}
 
 	return nil
